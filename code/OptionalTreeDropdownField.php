3415a4597964b9efbce33f95be3a979ff60db71d
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:10:59 +1300

    Converted to PSR-2

--- code/OptionalTreeDropdownField.php
+++ code/OptionalTreeDropdownField.php
@@ -11,81 +11,85 @@
 class OptionalTreeDropdownField extends TreeDropdownField
 {
 
-	private static $allowed_actions = array(
-		'tree'
-	);
+    private static $allowed_actions = array(
+        'tree'
+    );
 
-	/**
-	 * @var boolean $hasEmptyDefault Show the first <option> element as
-	 * empty (not having a value), with an optional label defined through
-	 * {@link $emptyString}. By default, the <select> element will be
-	 * rendered with the first option from {@link $source} selected.
-	 */
-	protected $hasEmptyDefault = false;
+    /**
+     * @var boolean $hasEmptyDefault Show the first <option> element as
+     * empty (not having a value), with an optional label defined through
+     * {@link $emptyString}. By default, the <select> element will be
+     * rendered with the first option from {@link $source} selected.
+     */
+    protected $hasEmptyDefault = false;
 
-	/**
-	 * @var string $emptyString The title shown for an empty default selection,
-	 * e.g. "Select...".
-	 */
-	protected $emptyString = '';
+    /**
+     * @var string $emptyString The title shown for an empty default selection,
+     * e.g. "Select...".
+     */
+    protected $emptyString = '';
 
-	/**
-	 * @param boolean $bool
-	 */
-	public function setHasEmptyDefault($bool) {
-		$this->hasEmptyDefault = $bool;
-		return $this;
-	}
+    /**
+     * @param boolean $bool
+     */
+    public function setHasEmptyDefault($bool)
+    {
+        $this->hasEmptyDefault = $bool;
+        return $this;
+    }
 
-	/**
-	 * @return boolean
-	 */
-	public function getHasEmptyDefault() {
-		return $this->hasEmptyDefault;
-	}
+    /**
+     * @return boolean
+     */
+    public function getHasEmptyDefault()
+    {
+        return $this->hasEmptyDefault;
+    }
 
-	/**
-	 * Set the default selection label, e.g. "select...".
-	 * Defaults to an empty string. Automatically sets
-	 * {@link $hasEmptyDefault} to true.
-	 *
-	 * @param string $str
-	 */
-	public function setEmptyString($str) {
-		$this->setHasEmptyDefault(true);
-		$this->emptyString = $str;
-		return $this;
-	}
+    /**
+     * Set the default selection label, e.g. "select...".
+     * Defaults to an empty string. Automatically sets
+     * {@link $hasEmptyDefault} to true.
+     *
+     * @param string $str
+     */
+    public function setEmptyString($str)
+    {
+        $this->setHasEmptyDefault(true);
+        $this->emptyString = $str;
+        return $this;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getEmptyString() {
-		return $this->emptyString;
-	}
+    /**
+     * @return string
+     */
+    public function getEmptyString()
+    {
+        return $this->emptyString;
+    }
 
-	/**
-	 * Get the whole tree of a part of the tree via an AJAX request.
-	 *
-	 * @param SS_HTTPRequest $request
-	 * @return string
-	 */
-	public function tree(SS_HTTPRequest $request) {
-		// Get tree from parent
-		$tree = parent::tree($request);
-		if($this->getHasEmptyDefault()) {
-			// Insert empty option into tree
-			$lf = "\n";
-			$tree = str_replace(
-				'<ul class="tree">'.$lf,
-				'<ul class="tree">'.$lf.
-					'<li id="selector-LinkInternalID-0" data-id="0" class="class-NoPageSelect closed"><a rel="0">'.
-						$this->getEmptyString().
-					'</a></li>'.$lf,
-				 $tree
-			);
-		}
-		return $tree;
-	}
-
+    /**
+     * Get the whole tree of a part of the tree via an AJAX request.
+     *
+     * @param SS_HTTPRequest $request
+     * @return string
+     */
+    public function tree(SS_HTTPRequest $request)
+    {
+        // Get tree from parent
+        $tree = parent::tree($request);
+        if ($this->getHasEmptyDefault()) {
+            // Insert empty option into tree
+            $lf = "\n";
+            $tree = str_replace(
+                '<ul class="tree">'.$lf,
+                '<ul class="tree">'.$lf.
+                    '<li id="selector-LinkInternalID-0" data-id="0" class="class-NoPageSelect closed"><a rel="0">'.
+                        $this->getEmptyString().
+                    '</a></li>'.$lf,
+                 $tree
+            );
+        }
+        return $tree;
+    }
 }
